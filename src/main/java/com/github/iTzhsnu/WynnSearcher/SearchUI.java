772436d39ccdbee37c55e5dbce7271e7dadee1ee
iTzhsnu <shunoosphere@outlook.jp>
2024-04-08 10:29:31 +0900

    change ver

--- src/main/java/com/github/iTzhsnu/WynnSearcher/SearchUI.java
+++ src/main/java/com/github/iTzhsnu/WynnSearcher/SearchUI.java
@@ -13,7 +13,7 @@ import java.util.*;
 import java.util.List;
 
 public class SearchUI extends JFrame implements ActionListener {
-    public static final String VERSION = "3.2.5";
+    public static final String VERSION = "3.2.6";
 
     //API
     private final List<JsonObject> wynnItems = new ArrayList<>();
